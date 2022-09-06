919e427066fcf7488cbf78ba08b9e0904be6d794
marcotuiio <marcotulio-barros@hotmail.com>
2022-09-06 13:12:39 -0300

    Lista de medidas de posição quase finalizada; updates

--- Lists/lista_medidas_posicao.R
+++ Lists/lista_medidas_posicao.R
@@ -71,7 +71,6 @@ media_bruta = MEDIA(glicemia); media_bruta
 mediana_bruta = (glicemia[10] + glicemia[11]) / 2; mediana_bruta;
 moda_bruta = 110
 
-rm(list=ls())
 # 2) Em um estudo sobre instituições de atendimento médico, foram obtidos dados 
 # da disponibilidade de leitos de 50 dessas instituições:
 
@@ -108,8 +107,6 @@ media = MEDIA(leitos); media
 mediana = MEDIANA(66, length(leitos), 24, 12, a); mediana
 moda = czuber(66, a, 1, 6); moda
 
-rm(list=ls())
-
 # 3) Os dados abaixo representam a largura máxima de amostras de crânios de 
 # animais machos. Calcule média, mediana e a moda. 
 cr = c(131, 119, 138, 125, 129, 126, 131, 132, 126, 128, 128, 131)
@@ -130,3 +127,62 @@ a = AT/k; a  # amplitude das classes
 intervalos = c("35|-60", "60|-85", "85|-110", "110|-135", "135|-160", "...")
 classe5_inf = 135;
 classe5_medio = (135+12 + 135+13) / 2; classe5_medio  # 24 elementos na classe
+
+# 5) Com base nos dados apresentados na Tabela 1, calcular a média, mediana e 
+# moda de ácido ascórbico
+
+fi = c(3, 6, 18, 35, 16, 2)
+xi = c(0.1, 0.3, 0.5, 0.7, 0.9, 1.1)  # ponto medio
+
+media = MEDIA_POND(fi, xi); media
+mediana = MEDIANA(0.6, 80, 27, 35, 0.2); mediana
+moda = czuber(0.6, 0.2, 17, 19); moda
+
+# 6) Consideremos 12 observações (ordenadas) do tempo de observações (dias) de 
+# animais de pequeno porte do Hospital Veterinário da UEL:
+
+observacoes = c(1, 4, 7, 9, 10, 13, 15, 17, 17, 18, 19, 21)
+# quartil = ?
+boxplot(observacoes)
+
+# 7) Com base nos dados da Tabela 2, calcule o peso médio dos ratos em cada 
+# idade. Usando o R, construa o gráfico de caixas para a variável peso, 
+# considerando-se cada rato (apenas um gráfico com todos os ratos).
+ 
+###############################################################################
+
+# 8) Trinta pessoas foram consultadas sobre sua cor favorita.
+# Construa uma tabela de frequências para os dados. Qual a escolha modal e a 
+# frequência relativa (ou porcentagem) desta cor?
+
+cores = c("vermelho", "azul", "verde", "amarelo", "preto", "marrom", "laranja", "roxo", "totais")
+fi = c(4, 11, 4, 3, 5, 1, 1, 1, 30);
+frp = c(fr(4, 30), fr(11, 30), fr(4, 30), fr(3, 30), fr(5, 30), fr(1, 30), fr(1, 30), fr(1, 30), 100)
+data = data.frame(cores, fi, frp); data
+# escolha_modal = azul, com 11 ocorrências
+azul_fr = frp[2]; azul_fr
+
+# 9) Calcule para a tabela de frequências abaixo sua respectiva média e mediana.
+xi = c(2, 3, 4, 5, 6);    # peso das frequencias 
+fi = c(3, 6, 10, 6, 3);   
+frp = c(fr(3, 28), fr(6, 28), fr(10, 28), fr(6, 28), fr(3, 28))
+Fi = c(3, 9, 19, 25, 28); # frequencias de cada valor
+Frp = c(fr(3, 28), fr(9, 28), fr(19, 28), fr(25, 28), fr(28, 28))
+data = data.frame(xi, fi, frp, Fi, Frp); data
+
+media_pond = MEDIA_POND(fi, xi); media_pond
+# mediana_bruta = 5;
+mediana = MEDIANA(4, sum(fi), 9, 10, 1); mediana
+
+# 10) Determinar o quarto decil, o primeiro quartil e o septuagésimo segundo 
+# percentil da seguinte distribuição
+classes = c("4|-9", "9|-14", "14|-19", "19|-24");
+fi = c(8, 12, 17, 3)
+frp = c(fr(8, 40), fr(12, 40), fr(17, 30), fr(3, 40))
+Fi = c(8, 20, 37, 40);
+Frp = c(fr(8, 40), fr(20, 40), fr(37, 40), fr(40, 40))
+data = data.frame(classes, fi, frp, Fi, Frp); data
+
+# quarto_decil = ?
+# primeiro_quartil = ?
+# septuagesimo_sefundo_percentil = ?
